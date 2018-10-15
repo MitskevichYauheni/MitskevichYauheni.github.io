5c195c39d18fcb64d764e4f00df823f374362536
Yauheni <mickevich.ea@gmail.com>
2018-10-15 21:32:58 +0300

    Update Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './base/styles/core.scss';
-import Travel from './components/travel';
-import Loc from './components/loc';
-import Profile from './components/profile';
-import NotFound from './components/not-found';
 import SideBar from './components/sidebar';
-
-const routes = [
-  {
-    path: '/',
-    exact: true,
-    component: Travel,
-  },
-  {
-    path: '/loc',
-    component: Loc,
-  },
-  {
-    path: '/profile',
-    component: Profile,
-  }
-];
+import ItemsSwitch from './switch';
 
 class App extends Component {
   render() {
     return (
       <Router >
         <div className='page'>
-          <Switch >
-            <Redirect from='/travel-app' to='/'/>
-
-            {routes.map((route, i) => (
-              <Route key={`route-${i}`} path={route.path} exact={route.exact} component={route.component} />
-            ))}
-            <Route component={NotFound} />
-          </Switch>
+          <ItemsSwitch />
           <SideBar />
         </div>
       </Router>
